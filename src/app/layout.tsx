03d4a997247af942db3540379a94c7a392cd5207
mishbahulmunir96 <misbahulmunir13@outlook.co.id>
2025-01-26 17:32:44 +0700

    fix: fixing error homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${poppins.className} ${amarante.className}antialiased`}>
         <NuqsProvider>
-          <ReactQueryProvider>{children}</ReactQueryProvider>
+          <ReactQueryProvider>
+            <Navbar />
+            {children}
+          </ReactQueryProvider>
         </NuqsProvider>
       </body>
     </html>
