04279b3a5d6b9e16e9d4fdaba132cec6485d819c
mishbahulmunir96 <157870568+mishbahulmunir96@users.noreply.github.com>
2025-02-01 14:56:24 +0700

    chores: adjust code transaction detail & transaction list (#13)

--- src/types/transaction.ts
+++ src/types/transaction.ts
@@ -1,15 +1,25 @@
 export interface Transaction {
   id: number;
   uuid: string;
+  userId: number;
   totalPrice: number;
+  paymentMethode: "MANUAL" | "OTOMATIS";
+  status:
+    | "WAITING_FOR_PAYMENT"
+    | "WAITING_FOR_PAYMENT_CONFIRMATION"
+    | "CANCELLED"
+    | "PROCESSED";
+  paymentProof: string | null;
+  checkInDate: string | null;
+  checkOutDate: string | null;
   duration: number;
-  createdAt: string;
-  updatedAt: string;
-  checkIn: string | null;
-  checkOut: string | null;
+  updatedAt: string | null;
   reservations: {
-    roomType: string;
+    roomType: "Deluxe" | "Standard" | "Suite";
+    roomPrice: number;
     propertyTitle: string;
     propertyLocation: string;
+    roomImages: string[];
+    roomFacilities: string[];
   }[];
 }
