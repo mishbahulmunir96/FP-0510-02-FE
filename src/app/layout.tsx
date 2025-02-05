9b61be6a59021aa987fb599ee2128095eb7a9bdf
andikalp313 <andikaluhurpambudi@gmail.com>
2025-02-05 22:58:09 +0700

    feat: property category management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,9 @@ import { ToastContainer } from "react-toastify";
 import "./globals.css";
 import { poppins } from "./utils/font";
 export const metadata: Metadata = {
-  title:
-    "MAKÉT | Makelar Ticket. Access Your Favorite Events with a Single Click!",
+  title: "RateHaven Property",
   description:
-    "Welcome to MAKÉT – Your Ultimate Event Management Solution! Whether you’re planning a personal celebration or a corporate gathering, MAKÉT is here to streamline your event management process. For Organizers: Effortlessly create, manage, and promote your events with our user-friendly platform. Collaborate with your team, track registrations, and sell tickets all in one place. For Attendees: Discover exciting events and enjoy a seamless registration experience. Stay updated with event details and connect with fellow attendees effortlessly. Join us at MAKÉT and make every event a memorable experience!",
+    "Welcome to RateHaven – Your Ultimate Event Management Solution! Whether you’re planning a personal celebration or a corporate gathering, MAKÉT is here to streamline your event management process. For Organizers: Effortlessly create, manage, and promote your events with our user-friendly platform. Collaborate with your team, track registrations, and sell tickets all in one place. For Attendees: Discover exciting events and enjoy a seamless registration experience. Stay updated with event details and connect with fellow attendees effortlessly. Join us at MAKÉT and make every event a memorable experience!",
 };
 
 export default function RootLayout({
