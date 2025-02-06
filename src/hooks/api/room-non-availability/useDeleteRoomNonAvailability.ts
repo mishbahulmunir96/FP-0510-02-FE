734541bc4d9b9f15f135e7853b51a578eda6f977
andikalp313 <andikaluhurpambudi@gmail.com>
2025-02-06 19:09:13 +0700

    feat: integrate room non availability management on frontend (#22)
    * fix
    
    * chore:user profile
    
    * feat: add room management hooks
    
    * feat: integrate room non availability management on frontend

---
+++ src/hooks/api/room-non-availability/useDeleteRoomNonAvailability.ts
@@ -0,0 +1,31 @@
+"use client";
+
+import useAxios from "@/hooks/api/useAxios";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+
+const useDeleteRoomNonAvailability = () => {
+  const router = useRouter();
+  const { axiosInstance } = useAxios();
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (id: number) => {
+      const { data } = await axiosInstance.delete(
+        `/roomNonAvailabilities/delete/${id}`,
+      );
+      return data;
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["roomNonAvailabilities"] });
+      toast.success("Delete Room Non Availability Success");
+    },
+    onError: (error: AxiosError<any>) => {
+      toast.error(error.response?.data);
+    },
+  });
+};
+
+export default useDeleteRoomNonAvailability;
